b914bab20f5d71ee4b973c94518c5a363f7541c6
a-h-designs <anthony.hancock85@gmail.com>
2024-04-26 12:06:46 +0100

    Initial Upload

--- src/test/java/com/advancedfishing/AdvancedFishingPluginTest.java
+++ src/test/java/com/advancedfishing/AdvancedFishingPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.advancedfishing;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class AdvancedFishingPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(AdvancedFishingPlugin.class);
 		RuneLite.main(args);
 	}
 }
